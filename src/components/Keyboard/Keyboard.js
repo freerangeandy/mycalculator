54c209cf77a84a9f4188b0761bc941000fed834c
freerangeandy <freerangeandy@users.noreply.github.com>
2019-08-11 19:17:04 -0400

    css modules

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import classes from './Keyboard.css';
+
 const keyboard = (props) => (
     <div>
-        <button onClick={props.entered}>Square</button>
+        <button
+            onClick={props.entered}
+            className={classes.Enter} >Square</button>
     </div>
 );
 
