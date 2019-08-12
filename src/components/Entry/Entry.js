a82e0e63cfdbb22af403690c7564f32fb5715853
freerangeandy <freerangeandy@users.noreply.github.com>
2019-08-12 18:28:28 -0400

    material-ui input field w/enter button

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ForwardIcon from '@material-ui/icons/Forward';
 
-const entry = (props) => (
-    <div>
-        <input
-                type="input"
+const useStyles = makeStyles({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4,
+  },
+});
+
+function Entry(props) {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.root}>
+            <InputBase
+                className={classes.input}
+                placeholder="asdfasd"
+                onChange={(event) => props.entryChanged(event)}
                 value={props.entryVal}
-                onChange={(event) => props.entryChanged(event)}/>
-    </div>
-);
+                onKeyPress={(event) => {
+                    if (event.key === 'Enter') {
+                        props.enterPressed(event);
+                    }
+                }}/>
+            <Divider className={classes.divider} />
+            <IconButton
+                color="primary"
+                className={classes.iconButton}
+                aria-label="directions"
+                onClick={(event) => props.enterPressed(event)} >
+              <ForwardIcon />
+            </IconButton>
+        </Paper>
+    )
+};
 
-export default entry;
+export default Entry;
