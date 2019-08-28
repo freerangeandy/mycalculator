f6c2347aeab6594eaf8ef0db2c5a78f1589b7724
freerangeandy <freerangeandy@users.noreply.github.com>
2019-08-28 15:50:10 -0400

    reference string-based symbols by key

--- src/shared/interpreter.js
+++ src/shared/interpreter.js
@@ -2,45 +2,47 @@ import nerdamer from 'nerdamer/nerdamer.core';
 import 'nerdamer/Algebra';
 import 'nerdamer/Calculus';
 
+import { KEYS } from './symbols';
+
 export const ARITHMETIC_OPERATORS = [
-    'divide',
-    'multiply',
-    'subtract',
-    'add',
+    KEYS.divide,
+    KEYS.multiply,
+    KEYS.subtract,
+    KEYS.add,
 ]; // ['∕', '×', '—', '+'];
 
 export const NUM_PAD = [
-    [7, 8, 9],             // [7, 8, 9],
+    [7, 8, 9],          // [7, 8, 9],
     [4, 5, 6],          // [4, 5, 6],
     [1, 2, 3],          // [1, 2, 3],
-    [0, '.', 'negate'], // [0, '.', '( − )'],
+    [0, '.', KEYS.negate], // [0, '.', '( − )'],
 ];
 
 export const FUNCTIONS = [
-    ['sum', 'integrate', 'diff', 'sqRoot'],         // ['∑', '∫', '∂', '√'],
-    ['sin' , 'cos' , 'tan' , 'naturalLog'],         // ['sin', 'cos', 'tan', 'ln'],
-    ['equals' , 'greaterThan' , 'lessThan' , ' '],  // ['=','>','<', ''],
-    ['(',')', ',', 'exponent'],                     // ['(',')', ',', '^'],
+    [KEYS.sum, KEYS.integrate, KEYS.diff, KEYS.sqRoot],  // ['∑', '∫', '∂', '√'],
+    [KEYS.sin, KEYS.cos, KEYS.tan, KEYS.naturalLog],     // ['sin', 'cos', 'tan', 'ln'],
+    [KEYS.equals, KEYS.greaterThan, KEYS.lessThan, ' '], // ['=','>','<', ''],
+    ['(',')', ',', KEYS.exponent],                       // ['(',')', ',', '^'],
 ];
 
 export const MODIFIERS = [
-    'secondFunc',
+    KEYS.secondFunc,
     '.',
     '.',
     '.',
 ];
 
 export const ACTIONS = [
-    'delete',
-    'answer',
-    'assign',
-    'clear',
+    KEYS.delete,
+    KEYS.answer,
+    KEYS.assign,
+    KEYS.clear,
 ];
 
 export const ALTERNATES = {
-    sin: 'asin',
-    cos: 'acos',
-    tan: 'atan',
+    [KEYS.sin]: KEYS.asin,
+    [KEYS.cos]: KEYS.acos,
+    [KEYS.tan]: KEYS.atan,
 }
 
 export const evalExpression = (expression) => {
