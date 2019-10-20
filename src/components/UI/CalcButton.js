3ad54374c7737f98ddb1221b11a3ba4242d85f32
freerangeandy <freerangeandy@users.noreply.github.com>
2019-10-20 15:05:28 -0400

    make media-responsive calc button size

--- src/components/UI/CalcButton.js
+++ src/components/UI/CalcButton.js
@@ -1,23 +1,48 @@
-// import React from 'react';
-import { withStyles} from '@material-ui/core/styles';
+import React from 'react';
+import { makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
 // dynamically size, for ipads, iphone5, galaxy s2
-const CalcButton = withStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     borderRadius: 0,
     boxShadow: 'none',
     textTransform: 'none',
-    fontSize: '90%',
     padding: '3px',
     border: '1px solid',
     lineHeight: '185%',
-    minWidth: 42,
-    maxWidth: 42,
-    minHeight: 36,
     '&:hover': {
       boxShadow: 'none',
     },
   },
-}))(Button);
+  iPad: {
+      fontSize: '90%',
+      minWidth: 63,
+      maxWidth: 63,
+      minHeight: 54,
+  },
+  phone: {
+      fontSize: '90%',
+      minWidth: 42,
+      maxWidth: 42,
+      minHeight: 36,
+  },
+  tinyPhone: {
+      fontSize: '80%',
+      minWidth: 36,
+      maxWidth: 36,
+      minHeight: 36,
+  },
+}));
+
+function CalcButton(props) {
+    const classes = useStyles();
+    // const isIPad = useMediaQuery('(min-height: 1000px)');
+    // const isLandscape = useMediaQuery('(orientation: landscape)');
+    const isTinyPhone = useMediaQuery('(max-width: 350px)');
+    const mediaClass = isTinyPhone ? classes.tinyPhone : classes.phone;
+    return <Button {...props} className={`${classes.root} ${mediaClass}`} />;
+}
 
 export default CalcButton;
