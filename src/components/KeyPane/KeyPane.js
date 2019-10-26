5b7aad172a5e1e49848448dafa4a8a8b2e97bf7a
Andy Lee <freerangeandy@gmail.com>
2019-10-26 17:15:16 -0400

    moved poppers to separate JS file

--- src/components/KeyPane/KeyPane.js
+++ src/components/KeyPane/KeyPane.js
@@ -2,28 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 // import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import blue from '@material-ui/core/colors/blue';
 
 import NumPad from './NumPad/NumPad';
 import ArithmeticPane from './ArithmeticPane/ArithmeticPane';
 import FunctionPane from './FunctionPane/FunctionPane';
 import ActionPane from './ActionPane/ActionPane';
 import paneClasses from './KeyPane.css';
-import { FUNCTIONS, MODIFIERS, ACTIONS, STAT_POP, MAT_POP } from '../../shared/buttonLayout';
-import { SYMBOLS } from '../../shared/symbols';
+import { FUNCTIONS, MODIFIERS, ACTIONS } from '../../shared/buttonLayout';
 import Auxy from '../../hoc/Auxy/Auxy';
-import InputPopper from '../UI/InputPopper';
-import SwitchPopper from '../UI/SwitchPopper';
-
-const popBgColor = blue[600];
-const popHoverColor = blue[700];
-
-const colorOverride = {
-    backgroundColor: popBgColor,
-    '&:hover': {
-      backgroundColor: popHoverColor,
-    }
-};
+import { getAllPoppers } from './Poppers/poppers';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,69 +20,10 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const getAllPoppers = (props, altPlacement) => {
-  const {tinySize, normalSize, tabletSize} = props.mediaQueries;
-  const assignPopper = {
-      component: (closeHandler) => (
-          <InputPopper
-              closeHandler={closeHandler}
-              submitHandler={(varName) => props.secondaryAction(SYMBOLS.assign.key, varName)}
-          />
-      ),
-      placement: altPlacement ? 'top-end' : 'left',
-  };
-
-  const modePopper = {
-      component: (closeHandler) => (
-          <SwitchPopper
-              closeHandler={closeHandler}
-              toggleDecimals={props.toggleDecimals}
-              useDecimals={props.useDecimals}
-              toggleDegrees={props.toggleDegrees}
-              useDegrees={props.useDegrees}
-          />
-      ),
-      placement: altPlacement ? 'bottom' : 'right',
-      styles: { // broken! make work again
-          fontSize: tinySize ? '68%' : normalSize ? '75%' : null,
-          paddingTop: tinySize ? '5px' : normalSize ? '3px' : null,
-      },
-  };
-
-  const statPopper = {
-      component: (closeHandler) => (
-          <FunctionPane
-              buttonPressed={props.buttonPressed}
-              gridValues={STAT_POP}
-              colorOverride={colorOverride}
-          />
-      ),
-      placement: altPlacement ? 'bottom' : 'right',
-  };
-
-  const matPopper = {
-      component: (closeHandler) => (
-          <FunctionPane
-              buttonPressed={props.buttonPressed}
-              gridValues={MAT_POP}
-              colorOverride={colorOverride}
-          />
-      ),
-      placement: altPlacement ? 'bottom' : 'right',
-  };
-
-  return {
-      [SYMBOLS.assign.key]: assignPopper,
-      [SYMBOLS.mode.key]: modePopper,
-      [SYMBOLS.stat.key]: statPopper,
-      [SYMBOLS.mat.key]: matPopper,
-  };
-}
-
 export default function KeyPane (props) {
     const classes = useStyles();
     const {keyPadBelow} = props.mediaQueries;
-    const allPoppers = getAllPoppers(props, keyPadBelow);
+    const allPoppers = getAllPoppers(props);
 
     const upperKeys = (
       <Auxy>
---
+++ src/components/KeyPane/Poppers/poppers.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import blue from '@material-ui/core/colors/blue';
+//not a react component
+import FunctionPane from '../FunctionPane/FunctionPane';
+import { STAT_POP, MAT_POP } from '../../../shared/buttonLayout';
+import InputPopper from '../../UI/InputPopper';
+import SwitchPopper from '../../UI/SwitchPopper';
+import { SYMBOLS } from '../../../shared/symbols';
+
+const popBgColor = blue[600];
+const popHoverColor = blue[700];
+
+const colorOverride = {
+    backgroundColor: popBgColor,
+    '&:hover': {
+      backgroundColor: popHoverColor,
+    }
+};
+
+export const getAllPoppers = (props) => {
+  const {tinySize, normalSize, tabletSize, keyPadBelow: altPlacement} = props.mediaQueries;
+  const assignPopper = {
+      component: (closeHandler) => (
+          <InputPopper
+              closeHandler={closeHandler}
+              submitHandler={(varName) => props.secondaryAction(SYMBOLS.assign.key, varName)}
+          />
+      ),
+      placement: altPlacement ? 'top-end' : 'left',
+  };
+
+  const modePopper = {
+      component: (closeHandler) => (
+          <SwitchPopper
+              closeHandler={closeHandler}
+              toggleDecimals={props.toggleDecimals}
+              useDecimals={props.useDecimals}
+              toggleDegrees={props.toggleDegrees}
+              useDegrees={props.useDegrees}
+          />
+      ),
+      placement: altPlacement ? 'bottom' : 'right',
+      styles: {
+          fontSize: tinySize ? '68%' : normalSize ? '75%' : null,
+          paddingTop: tinySize ? '5px' : normalSize ? '3px' : null,
+      },
+  };
+
+  const statPopper = {
+      component: (closeHandler) => (
+          <FunctionPane
+              buttonPressed={props.buttonPressed}
+              gridValues={STAT_POP}
+              colorOverride={colorOverride}
+          />
+      ),
+      placement: altPlacement ? 'bottom' : 'right',
+  };
+
+  const matPopper = {
+      component: (closeHandler) => (
+          <FunctionPane
+              buttonPressed={props.buttonPressed}
+              gridValues={MAT_POP}
+              colorOverride={colorOverride}
+          />
+      ),
+      placement: altPlacement ? 'bottom' : 'right',
+  };
+
+  return {
+      [SYMBOLS.assign.key]: assignPopper,
+      [SYMBOLS.mode.key]: modePopper,
+      [SYMBOLS.stat.key]: statPopper,
+      [SYMBOLS.mat.key]: matPopper,
+  };
+}
